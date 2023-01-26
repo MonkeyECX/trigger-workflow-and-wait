b8d980a4f5a7ada9d68e336b79391914e30200e9
Emerson <emerson.chalegre@monkey.exchange>
2023-01-26 11:10:10 -0300

    Fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -128,7 +128,7 @@ get_workflow_runs() {
 
   curl --fail-with-body -sSL \
       -X GET \
-      "${GITHUB_API_URL}/repos/${INPUT_OWNER}/${INPUT_REPO}/actions/workflows/${INPUT_WORKFLOW_FILE_NAME}/runs?${query}" \
+      "${GITHUB_API_URL}/repos/${INPUT_OWNER}/${INPUT_REPO}/actions/workflows/${INPUT_WORKFLOW_FILE_NAME}/runs" \
       -H "Authorization: Bearer ${INPUT_GITHUB_TOKEN}" \
       -H 'Accept: application/vnd.github.v3+json' 
       
