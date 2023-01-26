ee3c133fe3dde13a86ab7afd2d5685186519acee
Emerson <emerson.chalegre@monkey.exchange>
2023-01-26 11:10:10 -0300

    Fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -130,7 +130,7 @@ get_workflow_runs() {
 
   curl --fail-with-body -sSL \
       https://api.github.com/repos/MonkeyECX/monkey=spyros-argocd/actions/workflows/schedule.yml/runs \
-      -H 'Authorization: Bearer ${INPUT_GITHUB_TOKEN}' \
+      -H 'Authorization: Bearer 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' \
       -H 'Accept: application/vnd.github.v3+json' 
       
       #| jq -r '.workflow_runs[].id' | sort
